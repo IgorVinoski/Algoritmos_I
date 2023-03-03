81e79cc7f95a200deccb0c1a9830f52fbff6a2a6
Igor Vinoski <62020437+IgorVinoski@users.noreply.github.com>
2023-03-03 15:39:24 -0300

    Add files via upload

---
+++ Lista2/Exercicio2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+#include<cstdio>
+
+#include<cstdlib>
+
+using namespace std;
+
+/*
+Faça um algoritmo que leia um valor inteiro e apresente os resultados do quadrado e do cubo do
+valor lido.
+*/
+main()
+{
+    int number;
+
+    cout << "Write a number: ";
+    cin >> number;
+
+    cout << "The square of " << number << " are: " << number*number << "\nThe cube are: " << number*number*number;
+
+}
