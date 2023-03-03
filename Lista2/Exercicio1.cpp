a2500cc25a0eb300df9d6340d56bdff68b38e8ac
Igor Vinoski <62020437+IgorVinoski@users.noreply.github.com>
2023-03-03 15:34:54 -0300

    Lista2

---
+++ Lista2/Exercicio1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+#include<cstdio>
+
+#include<cstdlib>
+
+using namespace std;
+
+
+main()
+{
+/*
+Faça um programa que receba o preço de um produto, calcule e mostre o novo preço, sabendo-se
+que este sofreu um desconto de 10 %.
+*/
+
+     float price;
+
+     cout << "Enter product price : \n";
+     cin >> price;
+
+     cout << "The price after the discount: " << price*90/100;
+
+}
