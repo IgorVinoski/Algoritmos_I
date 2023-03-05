f9d4857173097894d77eedc8a04335212dc9e287
Igor Vinoski <62020437+IgorVinoski@users.noreply.github.com>
2023-03-05 18:34:28 -0300

    ex 10 11

---
+++ Lista2/Exercicio10.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+#include<cstdio>
+
+#include<cstdlib>
+
+using namespace std;
+
+/*
+Um plano de fidelidade acumula 1 ponto a cada R$ 5,00 gastos em compras nas lojas afiliadas. Faça
+um algoritmo para ler o valor de 4 compras feitas por um cliente e exibir quantos pontos ele
+acumulou no total.
+Observação: considere que o cliente pode acumular pontos fracionários.
+*/
+
+main()
+{
+
+int valor_compras;
+int fidelidade = 0;
+cout << "Informe o valor da: ";
+for(int i =0;i<4;i++){
+    cout << "Informe o valor da compra: ";
+    cin >> valor_compras;
+    fidelidade += valor_compras/5;
+}
+cout << "A quantidade de pontos é de: " << fidelidade;
+
+}
---
+++ Lista2/Exercicio11.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+
+#include<cstdio>
+
+#include<cstdlib>
+
+using namespace std;
+/*
+Uma locadora de carros precisa da sua ajuda para cobrar seus serviços. Escreva um programa que
+pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais
+ele foi alugado. Calcule o preço total a pagar, sabendo que o carro custa R$90 por dia e R$0,20 por
+Km rodado.
+*/
+
+main()
+{
+    float quant_km;
+    int dias;
+
+    float valor;
+
+    cout << "Digite a quantidade de quilometros rodados: ";
+    cin >> quant_km;
+
+    cout << "Digite a quantidae de dias: ";
+    cin >> dias;
+
+
+    valor = (quant_km*0.20) + (90*dias);
+
+    cout << "O valor é de: " << valor << " dinheiros";
+}
