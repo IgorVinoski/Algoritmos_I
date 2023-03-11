df6b43fe6d632a3efa23fdf7ac7d1982012b2597
Igor Vinoski <62020437+IgorVinoski@users.noreply.github.com>
2023-03-11 09:13:47 -0300

    lista 3

---
+++ Lista3/Exercicio3.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+
+using namespace std;
+
+/*
+
+Escreva um programa que leia a idade de um nadador, classifique-o em uma das seguintes
+categorias:
+IDADE CATEGORIA
+5 a 7 Infantil
+8 a 10 Infanto-juvenil
+11 a 13 Juvenil A
+14 a 17 Juvenil B
+Acima de 17 Adulto
+*/
+main()
+{
+
+    int idade;
+    cout << "Digita a idade do nadador: ";
+    cin >> idade;
+
+    if(idade >= 17){
+                cout << "Adulto";
+    }
+
+
+    switch(idade)
+    {
+        case 5 ... 7 :
+            cout << "INFANTIL";
+            break;
+        case 8 ... 10 :
+            cout << "INFANTIL";
+            break;
+        case 11 ... 13 :
+            cout << "INFANTIL";
+            break;
+        case 14 ... 17 :
+            cout << "INFANTIL";
+            break;
+        default :
+            cout << "IDADE INVÁLIDA";
+            break;
+
+    }
+}
---
+++ Lista3/Exercicio4.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+#include<cstdio>
+
+#include<cstdlib>
+
+using namespace std;
+/*
+4 - Escreva um programa que leia um número inteiro qualquer e verifique se o número é divisível por
+3 e por 5, o programa deverá apresentar como saída as palavras: “DIVISÍVEL” caso o número for
+divisível por 3 e por 5 ou “NÃO DIVISÍVEL” caso ele não for divisível. Construa também o diagrama de
+blocos deste exercício.
+
+*/
+
+main()
+{
+    int number;
+
+    cout << "Digite o número para verificar se ele é divisível por 3 e por 5: ";
+    cin >> number;
+
+    if(number%3==0 && number%5==0){
+        cout << "É divisível!";
+    } else {
+        cout << "Não é divisível!";
+    }
+
+}
