88a6b0e0f3c7e87652ed74af1c2cf690b6b4c5f7
Igor Vinoski <62020437+IgorVinoski@users.noreply.github.com>
2023-03-12 17:42:36 -0300

    ex 9 e 10

---
+++ Lista3/Exercicio10.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+#include<cstdio>
+
+#include<cstdlib>
+
+using namespace std;
+/*
+10 – Escreva um programa que receba três valores inteiros, A, B, C, verificar se eles podem ser valores
+dos lados de um triangulo e, se forem, se é um triângulo escaleno, equilátero ou isóscele, considerando
+os seguintes conceitos:
+• O comprimento de cada lado de um triangulo é menor do que a soma dos outros dois lados.
+• Chama-se equilátero o triângulo que tem três lados iguais.
+• Denominam-se isósceles o triângulo que tem o comprimento de dois lados iguais.
+• Recebe o nome de escaleno o triângulo que tem os três lados diferentes.
+*/
+
+main()
+{
+    int a, b, c;
+
+    cout << "Informe o valor de A: ";
+    cin >> a;
+    cout << "Informe o valor de B: ";
+    cin >> b;
+
+    cout << "Informe o valor de C: ";
+    cin >> c;
+
+
+    if((a+b) > c && (c+b) > a && (c+c) > b){
+        if(a==b && b==c){
+            cout << "Equilátero.";
+        }else if(a==b || a==c || c==b){
+            cout << "Isóceles. ";
+        }else{
+            cout << "Escaleno. ";
+        }
+    }else{
+        cout << "Não formam um triangulo. ";
+    }
+}
---
+++ Lista3/Exercicio9.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+#include<cstdio>
+
+#include<cstdlib>
+
+using namespace std;
+/*
+    9 – Escreva um programa que receba o valor bruto do salário de um funcionário e o valor da prestação
+de um empréstimo, em seguida o sistema deverá calcular o percentual que esta prestação representa
+no salário bruto e caso o percentual for menor ou igual à 25% do salário a consignação será concedida,
+caso contrário deverá exibir mensagem dizendo que o valor da prestação é maior que o permitido.
+*/
+
+main()
+{
+    float salario, prest_emprest;
+    cout << "Informe o salario: ";
+    cin >> salario;
+
+    cout << "Informe o valor da prestação do emprestimo: ";
+    cin >> prest_emprest;
+
+    if(prest_emprest/salario *100 <= 25){
+        cout << "Concedido";
+    }else{
+        cout << "Não concedido. ";
+    }
+}
