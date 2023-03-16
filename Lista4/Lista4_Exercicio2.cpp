ce88ad9902cac5942eccb27cfc6a7021057b5546
Igor Vinoski <62020437+IgorVinoski@users.noreply.github.com>
2023-03-16 16:46:01 -0300

    ex 2

---
+++ Lista4/Lista4_Exercicio2.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+
+using namespace std;
+/*
+2 – Escreva um algoritmo que leia 5 números, A, B, C, D e E. Mostrar o maior número e o menor número.
+*/
+
+main()
+{
+ int a, b, c, d, e;
+
+    cout << "Informe o valor de A: ";
+    cin >> a;
+    cout << "Informe o valor de B: ";
+    cin >> b;
+    cout << "Informe o valor de C: ";
+    cin >> c;
+    cout << "Informe o valor de D: ";
+    cin >> d;
+    cout << "Informe o valor de E: ";
+    cin >> e;
+
+
+
+    int num[5] = {a, b, c, d, e};
+    int maior, menor;
+
+    maior = a;
+    menor = a;
+    for(int i = 0; i<5; i++ ){
+        if(num[i]>maior){
+            maior = num[i];
+        }
+        if(num[i]<menor){
+            menor = num[i];
+        }
+    }
+    cout << "O menor valor é: " << menor << " O maior valor é: " << maior;
+}
